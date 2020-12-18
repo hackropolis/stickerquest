cbac20097d7d087c81799f1ae9fb8e74362aeb06
Charalampos Fanoulis <yo@fanoulis.dev>
2020-12-18 22:49:30 +0200

    chore: improved identation

--- prompts.rs
+++ prompts.rs
@@ -18,33 +18,33 @@ fn prompt() -> bool {
 pub fn intro() -> () {
     println!(
         "
-    ==================================
-               STICKER QUEST
-    ==================================
+	==================================
+			   STICKER QUEST
+	==================================
 
-    Huh, there's a vibration coming from
-    your pocket, and you wonder what causes
-    it.
+	Huh, there's a vibration coming from
+	your pocket, and you wonder what causes
+	it.
 
-    It's your phone. Obviously.
-    It's vibrating in your pocket.
+	It's your phone. Obviously.
+	It's vibrating in your pocket.
 
-    You pull out the phone out of your
-    pocket and pick it up instinctively.
+	You pull out the phone out of your
+	pocket and pick it up instinctively.
 
-    'I need you for a job.', says the person
-    on the other end of the line.
+	'I need you for a job.', says the person
+	on the other end of the line.
 
-    'Someone broke into our headquarters and
-    seems to have locked themselves in the
-    room where we package all the stickers!
-    If you happen to be nearby, could you please
-    check what's up?'. The caller sounds desperate.
+	'Someone broke into our headquarters and
+	seems to have locked themselves in the
+	room where we package all the stickers!
+	If you happen to be nearby, could you please
+	check what's up?'. The caller sounds desperate.
 
-    You're trying to make sense of why this
-    should matter to you, but it seems like
-    you're the only person who can deal
-    with this.
+	You're trying to make sense of why this
+	should matter to you, but it seems like
+	you're the only person who can deal
+	with this.
 
 	Do you accept?"
     );
@@ -53,13 +53,13 @@ pub fn intro() -> () {
     // const enw mporei na allaksei se allo function?
     // mhpws xanw kati apo scope?
     const LOSS_MSG: &str = "
-        You hang up on the caller. A few hours later,
-        you realize that your sticker order got cancelled.
-        
-        ... Someone apparently stole them. Gosh, this 
-        wouldn't have happened if you didn't decide to
-        not do anything! I can only imagine the same
-        you must be feeling right now.";
+		You hang up on the caller. A few hours later,
+		you realize that your sticker order got cancelled.
+		
+		... Someone apparently stole them. Gosh, this 
+		wouldn't have happened if you didn't decide to
+		not do anything! I can only imagine the same
+		you must be feeling right now.";
 
     if prompt() {
         how_to_unlock();
@@ -99,36 +99,37 @@ pub fn how_to_unlock() {
 	-----------------------------
 
 	The door's locked. On the other side,
-    you can hear someone typing frantically
-    as if their life depended on it.
+	you can hear someone typing frantically
+	as if their life depended on it.
 
-    Oh, you just heard a squeak! It sounds
-    vaguely familiar. \"Could it be?\",
-    you ask yourself.
+	Oh, you just heard a squeak! It sounds
+	vaguely familiar. \"Could it be?\",
+	you ask yourself.
 
-    There's a keycard lock next to the door,
-    but using the card you have on you may alert
-    the intruder. Alternatively, you could just
-    try to act fast and bruteforce your way
-    through. That'd be a bit loud, though.
+	There's a keycard lock next to the door,
+	but using the card you have on you may alert
+	the intruder. Alternatively, you could just
+	try to act fast and bruteforce your way
+	through. That'd be a bit loud, though.
 	
 	Do you use your card?"
     );
 
     const LOSS_MSG: &str = "
-        You scanned your keycard and the lock
-        rejected it. The intruder noticed and just
-        squeaked again, completely unbothered. They
-        know that you want to foil the plans, so
-        they swiftly pushed a bunch of heavy
-        objects in front of the door, before you
-        even managed to consider the alternative
-        option.
-        
-        The door got unlocked shortly afterwards,
-        with both the intruder and the stickers
-        having completely vanished, probably through
-        the backdoor.";
+		You scanned your keycard anyway...
+		and the lock rejected it. The intruder
+		noticed and just squeaked again,
+		completely unbothered. They know
+		that you want to foil the plans,
+		so they swiftly pushed a bunch of heavy
+		objects in front of the door, before you
+		even managed to consider the alternative
+		option.
+		
+		The door got unlocked shortly afterwards,
+		with both the intruder and the stickers
+		having completely vanished, probably through
+		the backdoor.";
 
     if prompt() {
         face_intruder();
@@ -140,7 +141,7 @@ pub fn how_to_unlock() {
 pub fn face_intruder() {
     println!(
         "
-	    -----------------------------
+		-----------------------------
 		|                           |
 		|                           |
 		|          _________        |
@@ -157,23 +158,22 @@ pub fn face_intruder() {
 		\"... Orpheus?\" you say, sounding horrified.
 		\"What are you doing here?\", you ask.
 
-		\"oh i'm just looking for stickers\", she
-		replies joyfully!
+		\"hrm!\", she replies joyfully.
 
-        \"..... uh, could you not?\"
+		\"..... uh, could you not?\"
 
-        Orpheus shrugs.
+		Orpheus shrugs.
 
-        \"eh okay but i just want stickerrrz\"
+		\"hrum\"
 
-        You hand over one of the sticker to Orpheus and
-        deescalate  the situation by convincing her that
-        using the same stickers all over the lid of her
-        laptop just won't look aesthetically pleasing
-        enough.
-        
-        Great job, you saved the day!
-    "
+		You hand over one of the sticker to Orpheus and
+		deescalate  the situation by convincing her that
+		using the same stickers all over the lid of her
+		laptop just won't look aesthetically pleasing
+		enough.
+		
+		Great job, you saved the day!
+	"
     );
 }
 
